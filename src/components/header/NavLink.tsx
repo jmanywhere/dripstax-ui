9e95a70707c9a13f3cb9abe7dd65bba78bbee0ad
Jose Miguel Fernandez <josmfernandez@gmail.com>
2022-10-12 12:37:33 -0600

    header nav styled

--- src/components/header/NavLink.tsx
+++ src/components/header/NavLink.tsx
@@ -1,10 +1,25 @@
 import Link from "next/link";
+import Image from "next/image";
 import type { FC } from "react";
 
-const NavLink: FC<{ href: string }> = ({ href }) => {
+const NavLink: FC<{
+  href: string;
+  name: string;
+  src: string;
+  alt: string;
+  img?: { h: number; w: number };
+}> = ({ href, name, src, alt, img }) => {
+  const height = 60;
+  const width = (60 * (img?.w || 381)) / (img?.h || 300);
   return (
-    <Link href={href}>
-      <a></a>
+    <Link href={href} passHref>
+      <a
+        onClick={(e) => e.preventDefault()}
+        className="flex flex-col items-center justify-center font-semibold py-1 px-2 mx-1 hover:bg-[rgba(255,255,255,0.05)] min-w-[95px]"
+      >
+        <Image width={width} height={height} src={src} alt={alt} />
+        {name}
+      </a>
     </Link>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 import Image from "next/image";
 // API
 import { trpc } from "../utils/trpc";
-// Images
+// Components
+import NavLink from "components/header/NavLink";
 
 const Home: NextPage = () => {
   const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
@@ -18,33 +19,90 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header className="px-4 py-2 flex items-center">
-        <Link href="/" passHref className="mr-2">
-          <a>
-            <Image
-              src="/logo/logo_white.png"
-              height={811 / 12}
-              width={783 / 12}
-              alt="Dripstax Logo"
-            />
-          </a>
-        </Link>
-        <nav>
-          <Link href="/swap">
-            <a onClick={(e) => e.preventDefault()}>SWAP</a>
+      <header className="px-8 py-2 flex flex-row fixed w-screen items-center justify-center">
+        <div className="flex-1">
+          <Link href="/" passHref className="mr-2">
+            <a>
+              <Image
+                src="/logo/logo_white.png"
+                height={811 / 12}
+                width={783 / 12}
+                alt="Dripstax Logo"
+              />
+            </a>
           </Link>
-          ACCOUNT REWARDS BANK LOTTERY VOTE
+        </div>
+        <nav className="flex items-center justify-center flex-1">
+          <NavLink
+            name="SWAP"
+            href="/swap"
+            src="/header/swap_1.png"
+            alt="swap_icon"
+          />
+          <NavLink
+            name="ACCOUNT"
+            href="/account"
+            src="/header/act_icon.png"
+            alt="account_icon"
+            img={{
+              h: 1210,
+              w: 1170,
+            }}
+          />
+          <NavLink
+            name="REWARDS"
+            href="/rewards"
+            src="/header/lock.png"
+            alt="lock_icon"
+            img={{
+              h: 359,
+              w: 300,
+            }}
+          />
+          <NavLink
+            name="BANK"
+            href="/bank"
+            src="/header/bank.png"
+            alt="bank_icon"
+            img={{
+              h: 1414,
+              w: 2000,
+            }}
+          />
+          <NavLink
+            name="LOTTERY"
+            href="/lottery"
+            src="/header/lottery.png"
+            alt="lottery_icon"
+            img={{
+              h: 1120,
+              w: 1120,
+            }}
+          />
+          <NavLink
+            name="VOTE"
+            href="/vote"
+            src="/header/vote.png"
+            alt="vote_icon"
+            img={{
+              h: 1000,
+              w: 1000,
+            }}
+          />
         </nav>
-        <div>CONNECT WALLET &amp; PRICE</div>
+        <div className="flex-1 flex justify-end">
+          CONNECT WALLET &amp; PRICE
+        </div>
       </header>
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-full p-4">
-        <Image
-          src="/bg/bg-dapp.jpg"
-          layout="fill"
-          objectFit="cover"
-          alt="dapp-bg"
-          className="z-[-1]"
-        />
+      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
+        <div className="absolute z-[-1] h-full w-full overflow-hidden">
+          <Image
+            src="/bg/bg_2.png"
+            layout="fill"
+            objectFit="cover"
+            alt="dapp-bg"
+          />
+        </div>
       </main>
     </>
   );
