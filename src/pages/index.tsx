2ce8580da2147982436115b544a41f93d3ac36ab
Jose Miguel Fernandez <josmfernandez@gmail.com>
2022-10-13 00:59:43 -0600

    changed logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,14 @@ const Home: NextPage = () => {
         <meta name="description" content="A Defi Revolution" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Image
-        src="/logo/logo-cr.png"
-        height={1090 / 4}
-        width={1414 / 4}
-        alt="color_logo"
-      />
+      <div className="max-h-[260px] overflow-visible relative z-[-1] top-[-100px]">
+        <Image
+          src="/logo/DRIPSTAX.svg"
+          height={500}
+          width={500}
+          alt="color_logo"
+        />
+      </div>
       <button className="rounded-full border-4 border-primary text-2xl px-14 py-4 font-light tracking-wider my-4 hover:bg-primary/25">
         Video Tutorials
       </button>
